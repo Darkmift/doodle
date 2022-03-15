22639141ad5b36baec2fa0d825053ff682f4b0d0
darkmift <darkmift@gmail.com>
2022-03-15 14:10:35 +0200

    1st commit

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,35 @@
+
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+	<div class="app">
+		<h1>Hi!</h1>
+		<code>{{ data ? data : "no data" }}</code>
+	</div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+<script setup lang="ts">
+import { onMounted, ref } from "vue";
 
-export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+const data = ref(null);
+
+onMounted(() => {
+	fetch("./config.json")
+		.then((res) => res.json())
+		.then((res) => {
+			data.value = res;
+		})
+		.catch((err) => {
+			console.log("🚀 ~ file: App.vue ~ line 20 ~ .then ~ err", err);
+		});
 });
 </script>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
 }
 </style>
